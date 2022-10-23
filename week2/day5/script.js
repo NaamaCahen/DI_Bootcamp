42dd42703d72c9b80f8ccdf1fd1253c525eef6dc
NaamaCahen <cahenbb@gmail.com>
2022-10-23 12:53:18 +0300

    finish the exercise

--- week2/day5/script.js
+++ week2/day5/script.js
@@ -5,13 +5,45 @@ function playTheGame(){
         alert(`No problem, Goodbye`);
     }
     else{
-        let userNum= +prompt(`please enter a number between 1 and 10`);
-        while(isNaN(userNum) || userNum===0 ){
-            let userNum= +prompt(`PLEASE ENTER A NUMBER BETWEEN 1 AND 10`);
-        }
-        while(userNum<0 || userNum>10){
-            let userNum= +prompt(`PLEASE ENTER A NUMBER BETWEEN 1 AND 10`);
-        }
-        let computerNumber = Math.round( Math.random()*10);
+        let computerNumber = Math.round( (Math.random()*10)+1);
+        guess(computerNumber,0);
+    }
+}
+function guess(computerNumber,counter){
+    counter++;
+    let userNum= +prompt(`please enter a number between 1 and 10`);
+    while(isNaN(userNum) || userNum===0 ){
+         userNum= +prompt(`PLEASE ENTER A NUMBER BETWEEN 1 AND 10`);
+    }
+    while(userNum<0 || userNum>10){
+         userNum= +prompt(`PLEASE ENTER A NUMBER BETWEEN 1 AND 10`);
+    }
+    
+    compareNumbers(userNum,computerNumber,counter);
+}
+
+//second part
+
+function compareNumbers(userNumber,computerNumber,counter) {
+   
+    if(userNumber===computerNumber){
+        alert(`WINNER!`)
+        return ; 
+    }else if(userNumber>computerNumber && counter<=4){
+        alert(`Your number is bigger then the computer's, guess again `);
+       
+        counter++;
+        guess(computerNumber,counter);
+       
+    }else if(userNumber<computerNumber && counter<=4){
+        alert(`Your number is smaller then the computer's, guess again`);
+        
+        counter++;
+        guess(computerNumber,counter);
+        
+    }
+    else if(counter>3){
+        alert(`out of chances`);
+        return;
     }
 }
