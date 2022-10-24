0a9dfac74d2f8c8731bd03755e0620641653d250
NaamaCahen <cahenbb@gmail.com>
2022-10-24 16:04:26 +0300

    attributes dom

--- week3/day1/script.js
+++ week3/day1/script.js
@@ -9,3 +9,37 @@ console.log(divUser.nextElementSibling);//2nd way
 //3.
 console.log(document.body.children[1].children[1]);
 console.log(divUser.nextElementSibling.firstElementChild.nextElementSibling);
+
+
+
+
+
+// Add a <div id="container"></div> to the HTML file
+// Create a function that adds the name of each students to a paragraph
+// each paragraph needs to be background yellow, font-size 25px
+// Add the 3 paragraph to the div already on the page
+
+
+//lise's solution:
+
+const users = ["John", "Lola", "Tom"];
+
+// for each user 
+// create a paragrah
+// the text of the paragraph is the name of the user
+
+function addPara () {
+    const wrapper = document.getElementById("container"); 
+    for (let i = 0; i<users.length; i++){
+        const paragraph = document.createElement("p");
+        paragraph.classList.add("para"); // add a class called para
+        const text = document.createTextNode(`Hello ${users[i]}`);
+        paragraph.appendChild(text);
+        wrapper.appendChild(paragraph);
+    }
+}
+
+addPara();
+
+document.getElementById("container").style.backgroundColor = "yellow"
+// inline style
